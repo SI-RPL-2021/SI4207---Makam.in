75b6e2e7e9a8835f7c9e418c76b0f8150e3c3661
NurmalaNurmala <malanur542@gmail.com>
2021-03-24 12:29:14 +0700

    progress admin dan verifikasi ref #7

---
+++ admin/homeadmin.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Makam.in</title>
+	<link rel="icon" href="../assets/img/icon.png" type="image/gif">
+	<link rel="stylesheet" type="text/css" href="../assets/css/homeadmin.css">
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../assets/jquery/jquery-3.6.0.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .navbar-nav li{
+            background-color: red
+            margin-left:30px;
+            margin-right:30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container" style="font-family: 'Quicksand';font-weight: bold">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid" >
+            <img src="../assets/img/icon.png" style="width: 30px">
+            <a class="navbar-brand" href="#">Makam.in</a>
+            <div class="collaps navbar-collapse justify-content-between">
+                <ul class="navbar-nav me-auto">
+                    
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link" href="homeadmin.php" style="border-bottom: 2px solid ;border-bottom-color: #4CB051">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="verifikasi.php">Verifikasi</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link " href="#" >Selamat Datang, <span style="color: #4CB051">Reza!</span></a>
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </nav>
+    </div>
+	<!-- <?php 
+    $varKet = 'home';
+    include('navbar_sglogin.php'); 
+    ?> -->
+	<div class="InfoSec" style="font-family: 'Quicksand'"> 
+        <div class="container">
+            <div class="InfoRow">
+                <div class="InfoColumn">
+                    <div class="ImgWrapper">
+                        <img src="../assets/img/admin/homeadmin.png">
+                    </div>
+                </div>
+                <div class="InfoColumn">
+                    <div class="TextWrapper">
+                        <div class="Heading">Bantu Mereka Urus Sewa Lahan Makam</div>
+                        <div class="Subtitle">Sewa hingga perpanjang makam dapat langsung didaftarkan secara online tanpa harus bolak-balik</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ admin/signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Makam.in</title>
+	<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="assets/css/signup.css">
+	<link rel="icon" href="assets/img/icon.png" type="image/gif">
+</head>
+<body>
+	<?php include('navbar_sglogin.php'); ?>
+	<div class="InfoSec" style="font-family: 'Quicksand'"> 
+        <div class="container">
+            <div class="InfoRow">
+                <div class="InfoColumn">
+                    <div class="TextWrapper">
+                        <div class="Heading">Urus izin dan sewa lahan makam jadi lebih mudah</div>
+                        <div class="Subtitle">Sewa hingga perpanjang makam dapat langsung didaftarkan secara online tanpa harus bolak-balik</div>
+                        <div class="button1">
+                        	<button type="button" class="btn btn-success rounded-pill">Info Lebih lanjut</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="InfoColumn">
+                    <div class="ImgWrapper">
+                        <img src="assets/img/signup.png">
+                    </div>
+                    <div class="TextWrapper" style="color: black">
+	                    <div class="Heading2">Sewa lahan makam udah hanya di Makam.in</div>
+	                    <div class="Subtitle2">Gabung dan nikmati kemudahan mengurus izin lahan makam</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<!-- <div class="container" style="height: 90vh">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<img src="assets/img/landing.png" style="width: 100%;margin-top: 7vh">
+			</div>
+			<div class="col-md-6">
+			</div>
+		</div>
+	</div> -->
+</body>
+</html>
