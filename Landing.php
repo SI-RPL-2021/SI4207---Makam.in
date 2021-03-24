c01689188065cd09549b50cdb620edc27f79621f
dwikurnianto <dwikurniantou@gmail.com>
2021-03-24 11:49:23 +0700

    progress PBI 1 (membuat landing, login, dan sign up page) ref #8

---
+++ Landing.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>MAKAM.IN!</title>
+</head>
+
+<body>
+    <!-- Container Website -->
+    <div class="container-md-10">
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <title>Bootstrap Example</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        </head>
+
+        <body>
+            <div class="container-fluid">
+                <!-- Brand/logo -->
+                <div class="row">
+                    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                        <div class="col-sm-3">
+                            <a class="navbar-brand" href="#">
+                                <img src="logo makam.png" alt="logo" style="width:50px;">
+                                Makam.in
+                            </a>
+                        </div>
+                        <div class="col-sm-6">
+                            <!-- Links -->
+                            <ul class="navbar-nav justify-content">
+                                <div class="col-sm ">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark" href="#">Home</a>
+                                    </li>
+                                </div>
+                                <div class="col-sm">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark" href="#">Sewa</a>
+                                    </li>
+                                </div>
+                                <div class="col-sm">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark" href="#">Perpanjang</a>
+                                    </li>
+                                </div>
+                                <div class="col-sm">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark" href="#">Bayar</a>
+                                    </li>
+                                </div>
+                            </ul>
+                        </div>
+                        <div class="col-md">
+                            <div class="d-grid gap-2 d-md-flex justify-content">
+                                <button class="btn btn-outline-success me-md-2" type="button">Daftar</button>
+                                <button class="btn btn-success" type="button">Masuk</button>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <img src="grave.png" class="img-fluid" alt=" " style="width:500; height:700">
+                    </div>
+                    <div class="col">
+                        <p class="text-end fw-bold fs-1">Urus izin dan sewa</p>
+                        <p class="text-end fw-bold fs-1">lahan makam jadi</p>
+                        <p class="text-end fw-bold fs-1">lebih mudah</p>
+                        <br>
+                        <p class="text-end fw-light fs-4">Sewa hingga perpanjang makam dapat langsung</p>
+                        <p class="text-end fw-light fs-4">didaftarkan secara online tanpa harus</p>
+                        <p class="text-end fw-light fs-4">bolak-balik</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </body>
+
+        </html>
+
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
